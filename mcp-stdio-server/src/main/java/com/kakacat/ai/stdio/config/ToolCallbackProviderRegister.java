74596d7a6826c67f24bb280f87e71f0ab40bbc4e
yunp <yunp>
2025-04-11 15:14:33 +0800

    mcp

--- mcp-stdio-server/src/main/java/com/kakacat/ai/stdio/config/ToolCallbackProviderRegister.java
+++ mcp-stdio-server/src/main/java/com/kakacat/ai/stdio/config/ToolCallbackProviderRegister.java
@@ -1,14 +1,11 @@
 package com.kakacat.ai.stdio.config;
 
+import com.kakacat.ai.stdio.mcp.RuntimeEnvironmentService;
 import com.kakacat.ai.stdio.mcp.WeatherService;
-import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 
 @Configuration
@@ -17,5 +14,7 @@ public class ToolCallbackProviderRegister {
     public ToolCallbackProvider weatherTools(WeatherService weatherService) {
         return MethodToolCallbackProvider.builder().toolObjects(weatherService).build();
     }
+
+
 
 }
